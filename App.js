f83be1acded52072e5457c62a57386253bc60c5a
Arifular <arifulfarhan75@gmail.com>
2022-02-13 16:33:53 +0700

    Finishing

--- App.js
+++ App.js
@@ -5,17 +5,6 @@ import { View, Text } from "react-native";
 const App = () => {
   return(
     <Navigation />
-    // <View style={{
-    //   padding: 20,
-    //   backgroundColor: "#C4C4C4",
-    // }}>
-    //   <Text style={{
-    //     fontSize: 20,
-    //     color: "#000",
-    //     fontWeight: "bold",
-    //     textAlign: "center",
-    //   }}>Belum ada Transaksi</Text>
-    // </View>
   )
 }
 
